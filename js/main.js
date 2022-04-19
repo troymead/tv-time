ace39a12d747274b1992c3f6c6daf14a82e3531a
Faith Tremmel <tremmefr@mail.uc.edu>
2022-04-19 14:54:36 -0400

    Added word cloud

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// import * as d3 from "https://cdn.skypack.dev/d3@7";
 
 d3.csv('data/all_data.csv')
 .then(data => {
@@ -11,8 +10,10 @@ d3.csv('data/all_data.csv')
             }
         }
 
-        console.log(d)
+        // console.log(d)
 
     })
+
+    wordCloud = new WordCloud({ parentElement: '#word-cloud'}, data)
 })
 .catch(error => console.error(error));
