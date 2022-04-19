823aba5bc5a361d178065278fdb5dcde22f985c3
antiriby <brianaantiri10@gmail.com>
2022-04-19 16:17:31 -0400

    Add barcharts for lines per character

--- js/main.js
+++ js/main.js
@@ -1,31 +1,43 @@
 // import * as d3 from "https://cdn.skypack.dev/d3@7";
 
+characterData = []
+totalData = []
+let characterGroups
+let barChartData = []
 d3.csv('data/all_data.csv')
 .then(data => {
-
+    console.log(data)
     data.forEach((d) => {
 
         for (let key in d) {
             if (/\d/.test(d[key])) {
-                d[key] = +d[key]; // convert any numbers present to ints
+                d[key] +d[key]; // convert any numbers present to ints
             }
         }
 
-        console.log(d);
 
         // TODO: restructure data for plotting on charts - use d3.groups
-        let characterGroups = d3.groups(data, d => d.Character);
+        characterGroups = d3.groups(data, d => d.Character);
 
-        let sznEps = {};
+        // let sznEps = {};
 
-        let szn1Eps = d3.groups(data, d => d.S1Eps);
+        // let szn1Eps = d3.groups(data, d => d.S1Eps);
 
-        console.log(characterGroups);
-        console.log(szn1Eps);
+        // console.log(characterGroups);
+        // console.log(szn1Eps);
 
     })
 
-    console.log(data);
+    characterData = characterGroups.slice(0,-1);
+    totalData = characterGroups.slice(-1);
+    totalData = totalData[0][1]
+    characterData.forEach(d => barChartData.push(d[1][0]));
+
+    let lines = new BarChart({
+        'parentElement': "#wordLines",
+        'containerHeight': 400, 
+        'containerWidth': 600,
+    }, [barChartData, totalData])
 
 })
 .catch(error => console.error(error));
